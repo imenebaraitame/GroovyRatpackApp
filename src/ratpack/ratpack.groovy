7d83967d1e79c341ccaf4d1575b6792ebb2b83b0
imenebaraitame <baraitame.imene@gmail.com>
2021-11-19 12:24:26 +0100

    update ratpack.groovy

--- src/ratpack/ratpack.groovy
+++ src/ratpack/ratpack.groovy
@@ -1,6 +1,7 @@
 import app.model.PhotoService
 import app.services.DefaultPhotoService
 import ratpack.form.Form
+import ratpack.path.PathTokens
 import ratpack.thymeleaf3.ThymeleafModule
 import static ratpack.thymeleaf3.Template.thymeleafTemplate
 import static ratpack.groovy.Groovy.ratpack
@@ -22,12 +23,11 @@ ratpack {
 
             }
             get(":name"){
-                PhotoService photoService ->
-                    response.sendFile(photoService.get(pathTokens.name))
+               PhotoService photoService ->
+                   response.sendFile(photoService.get(pathTokens.name))
             }
         }
         get("show/:name"){
-
             name:getPathTokens().get("name")
             render thymeleafTemplate("photo")
 
