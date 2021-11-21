40d5f6a77edd967221df0effadbd56c6ef74b943
imenebaraitame <baraitame.imene@gmail.com>
2021-11-21 19:45:41 +0100

    fix conflicts

---
+++ build/resources/main/ratpack.groovy
@@ -0,0 +1,38 @@
+import app.model.PhotoService
+import app.services.DefaultPhotoService
+import ratpack.form.Form
+import ratpack.thymeleaf3.ThymeleafModule
+import static ratpack.thymeleaf3.Template.thymeleafTemplate
+import static ratpack.groovy.Groovy.ratpack
+
+ratpack {
+    bindings {
+        module (ThymeleafModule)
+        bind (PhotoService, DefaultPhotoService)
+
+    }
+    handlers {
+        prefix("photo"){
+            post {
+                PhotoService photoService ->
+                    parse(Form.class).then({ form ->
+                    def name = photoService.save(form.file("photo"))
+                    redirect "/show/$name"
+                })
+
+            }
+            get(":name"){
+               PhotoService photoService ->
+                   response.sendFile(photoService.get(pathTokens.name))
+            }
+        }
+        get("show/:name"){
+
+            def name = getPathTokens().get("name")
+            render( thymeleafTemplate("photo", ['name': name]) )
+        }
+
+        files { dir "public" indexFiles 'index.html' }
+
+    }
+}
