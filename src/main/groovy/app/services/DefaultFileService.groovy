e0f1e140366ed931fc5c6c5325d82c9f84fd853b
imenebaraitame <baraitame.imene@gmail.com>
2021-12-21 20:06:13 +0100

    use mime type to get the content type of a file

--- src/main/groovy/app/services/DefaultFileService.groovy
+++ src/main/groovy/app/services/DefaultFileService.groovy
@@ -7,21 +7,24 @@ import java.nio.file.Path
 
 class DefaultPhotoService implements PhotoService {
 
-    private static final PREFIX = "ratpack-"
+    private static final prefix = "ratpack-"
     final Path tmpDir = File.createTempDir().toPath()
 
     @Override
     String save(UploadedFile f, String uploadPath) {
-        String ext = getSuffix(f)
-        Path dest = Files.createTempFile(tmpDir, PREFIX, ext)
-        String fileId = dest.fileName.toString().replaceAll("^${PREFIX}", "").replaceAll("${ext}\$", "")
+        String suffix = getSuffix(f)
+        Path dest = Files.createTempFile(tmpDir, prefix, suffix)
+        String fileId = dest.fileName.toString().replaceAll("^${prefix}", "")
+
         def unixPath = Files.write(dest, f.bytes)
         println("unixPath: ${unixPath}, fileId: ${fileId}")
+        File outputFile = new File(uploadPath, "${fileId}")
 
-        File outputFile = new File(uploadPath, "${fileId}${ext}")
+
         f.writeTo(outputFile.newOutputStream())
         println("outputFile: ${outputFile.path}, Exists: ${outputFile.exists()}")
         return fileId
+
     }
 
 
@@ -37,7 +40,7 @@ class DefaultPhotoService implements PhotoService {
     }
 
     private static String getFileName(String name,UploadedFile f) {
-       return "${PREFIX}${name}${getSuffix(f)}"
+       return "${prefix}${name}"
     }
 
 }
--- src/ratpack/ratpack.groovy
+++ src/ratpack/ratpack.groovy
@@ -1,11 +1,13 @@
-import app.model.PhotoService
-import app.services.DefaultPhotoService
+import app.model.FileService
+import app.services.DefaultFileService
 import ratpack.form.Form
 import ratpack.thymeleaf3.ThymeleafModule
+
 import static ratpack.thymeleaf3.Template.thymeleafTemplate
 import static ratpack.groovy.Groovy.ratpack
 import ratpack.server.BaseDir
 import java.nio.file.Paths
+import ratpack.file.MimeTypes
 
 def uploadDir = 'uploads'
 def publicDir = 'public'
@@ -21,45 +23,52 @@ def uploadPath = baseDir.resolve(uploadDir)
 ratpack {
     bindings {
         module (ThymeleafModule)
-        bind (PhotoService, DefaultPhotoService)
+        bind (FileService, DefaultFileService)
     }
     handlers {
-        prefix("photo"){
+        prefix("upload"){
             post {
-                PhotoService photoService ->
+                FileService fileService ->
                     parse(Form.class).then({ def form ->
-                    def f = form.file("photo")
-                    def name = photoService.save(f, uploadPath.toString())
-                    String suffix = photoService.getSuffix(f)
-                    redirect "/show/$name/$suffix"
+                    def f = form.file("upload")
+                    def name = fileService.save(f, uploadPath.toString())
+                        def contentType = context.get(MimeTypes).getContentType(name)
+
+                      if(contentType.contains("application/pdf"))
+                       {
+                           redirect "/show/$name"
+                       }else{
+                          redirect "/appear/$name"
+                       }
+
                 })
             }
             get(":name"){
-               PhotoService photoService ->
+                FileService fileService ->
                 parse(Form.class).then({ def form ->
-                    def f = form.file("photo")
-                    response.sendFile(photoService.get(pathTokens.name, f))
+                    def f = form.file("upload")
+                    response.sendFile(fileService.get(pathTokens.name, f))
                 })
             }
         }
 
-        get('image/:id'){
-            def imagePath = new File("${uploadPath}/${pathTokens['id']}")
-            // you'd better check if the image exists...
-            println("imagePath: ${imagePath}, exists: ${imagePath.exists()}")
-            render Paths.get(imagePath.toURI())
+        get('file/:id'){
+            def filePath = new File("${uploadPath}/${pathTokens['id']}")
+            // you'd better check if the file exists...
+            println("filePath: ${filePath}, exists: ${filePath.exists()}")
+            render Paths.get(filePath.toURI())
         }
 
-        get("show/:name/:suffix"){
+        get("show/:name"){
             String fileId = getPathTokens().get("name")
-            String SU = getPathTokens().get("suffix")
-            String path = "/image/${fileId}${SU}"
+            String path = "/file/${fileId}"
+            render( thymeleafTemplate("pdf", ['fullpath': path]) )
 
-            if (SU in [".jpg", ".jpeg", ".png"]) {                
-                render( thymeleafTemplate("photo", ['fullpath': path]) )
-            } else{
-                render( thymeleafTemplate("pdf", ['fullpath': path]) )
-            }
+        }
+        get("appear/:name"){
+            String fileId = getPathTokens().get("name")
+            String path = "/file/${fileId}"
+            render( thymeleafTemplate("photo", ['fullpath': path]) )
         }
 
         files { dir "public" indexFiles 'index.html' }
